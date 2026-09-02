1395b05bbe967685e359bf1fdad2d5233e3121bf
EdvinJuu <edvin.juujarvi@hotmail.com>
2026-09-02 10:42:43 +0200

    La till kommentar i dateFormatter

--- src/utilities/dateFormatter.js
+++ src/utilities/dateFormatter.js
@@ -3,3 +3,5 @@
 const reformulateDate = ({date, time}) => {
     return `<p> Din bokade tid är: ${date} kl ${time}</p>` 
 }
+
+// Vill ha från reformulateDate dag, timme fint utskrivet. Och ett objekt som har exakt tid för att stoppa konflikter(om två bokar bord samtidigt)
